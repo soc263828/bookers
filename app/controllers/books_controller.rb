80c3954b63c80d390eb57b2671dfd000e5e269cd
YukaHirai <133447397+yusoraka@users.noreply.github.com>
2023-06-27 13:05:56 +0000

    flash

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,23 +4,29 @@ class BooksController < ApplicationController
   end
 
   def create
-
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+   @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book.id)
+    else
+    @books = Book.all
+    render :index
+    end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
-   
+
   end
 
   def edit
     @book = Book.find(params[:id])
+    flash[:notice] = "Book was successfully created."
   end
 
 def update
@@ -37,6 +43,6 @@ end
 
   private
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
