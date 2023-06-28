b34584018bb0805ff1026b80b14a6f949b14e60b
YukaHirai <133447397+yusoraka@users.noreply.github.com>
2023-06-28 09:57:05 +0000

    fin

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -31,13 +31,13 @@ class BooksController < ApplicationController
 
 def update
   @book = Book.find(params[:id])
-  if @book.save
+  if @book.update(book_params)
       flash[:notice] = "Book was successfully created."
     redirect_to book_path(@book.id)
-    else
+  else
     @books = Book.all
   render :edit
-  end
+   end
 end
 
 def destroy
